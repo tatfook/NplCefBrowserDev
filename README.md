41a59936b44cb081b672603b272a937731aff56d
leio zhang <leio zhang>
2016-11-25 15:15:52 +0800

    changed deps folder

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # NplCefBrowserDev
 ### Build NplCefBrowser on Windows
 - Set BOOST_ROOT environment
-- Download or Git clone [NPLRuntime](https://github.com/LiXizhi/NPLRuntime.git) into npl_packages/NPLRuntime
-- Download cef3 binary [Branch 2704-Windows 32bit](https://cefbuilds.com/) and unzip to cef3
-- Run build.bat 
+- Download [NPLRuntime](https://github.com/LiXizhi/NPLRuntime.git) and unzip into:[deps/NPLRuntime]
+- Download cef3 binary [Branch 2704-Windows 32bit](https://cefbuilds.com/) and unzip into:[deps/cef3]
+- Open Developer Command Prompt for VS2015 and run build.bat 
 - Setp 1: Build libcef_dll_wrapper.lib
 - Setp 2: Build NplCefBrowser.dll
-- NplCefBrowser.dll will be located at folder:ParaWorld
+- NplCefBrowser.dll will be located at folder: ../ParaWorld
 ### Main Folder's structure
 ```lua
-NplCefBrowser
-----npl_packages/NPLRuntime
-----cef3
-----ParaWorld
+        NplCefBrowser
+                ----deps/NPLRuntime
+                ----deps/cef3
+ParaWorld
 ```
--- build.bat
+++ build.bat
@@ -1,20 +1,20 @@
 @echo off 
 rem Setp 1: Build libcef_dll_wrapper.lib
-if not exist "cef3/bin/Win32" ( mkdir "cef3/bin/Win32" )
+if not exist "deps/cef3/bin/Win32" ( mkdir "deps/cef3/bin/Win32" )
 
-pushd "cef3/bin/Win32"
+pushd "deps/cef3/bin/Win32"
     cmake -G "Visual Studio 14 2015" ../../
     msbuild cef.sln /p:Configuration=Debug
     msbuild cef.sln /p:Configuration=Release
 popd
-copy .\cef3\bin\Win32\libcef_dll_wrapper\Debug\libcef_dll_wrapper.lib .\cef3\Debug\libcef_dll_wrapper.lib 
-copy .\cef3\bin\Win32\libcef_dll_wrapper\Release\libcef_dll_wrapper.lib .\cef3\Release\libcef_dll_wrapper.lib
+copy .\deps\cef3\bin\Win32\libcef_dll_wrapper\Debug\libcef_dll_wrapper.lib .\deps\cef3\Debug\libcef_dll_wrapper.lib 
+copy .\deps\cef3\bin\Win32\libcef_dll_wrapper\Release\libcef_dll_wrapper.lib .\deps\cef3\Release\libcef_dll_wrapper.lib
 
 rem Setp 2: Build NplCefBrowser.dll
 if not exist "bin/Win32" ( mkdir "bin/Win32" )
 
 pushd "bin/Win32"
-    cmake -G "Visual Studio 14 2015" -DParaEngineClient_SOURCE_DIR:STRING=.\npl_packages\NPLRuntime\Client\trunk\ParaEngineClient  ../../
+    cmake -G "Visual Studio 14 2015" -DParaEngineClient_SOURCE_DIR:STRING=.\deps\NPLRuntime\Client\trunk\ParaEngineClient   ../../
     msbuild NplCefBrowser.sln /p:Configuration=Debug
     msbuild NplCefBrowser.sln /p:Configuration=Release
 popd
